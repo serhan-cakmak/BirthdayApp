9c927a814fd195e8528f8889689974f7dc41f8db
serhan-cakmak <104589882+serhan-cakmak@users.noreply.github.com>
2022-08-23 14:36:26 +0300

    backend first
    spring downloaded

---
+++ BirthdayAppB/birthdayapp/src/main/java/com/serbay/birthdayapp/BirthdayappApplication.java
@@ -0,0 +1,13 @@
+package com.serbay.birthdayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayappApplication.class, args);
+	}
+
+}
---
+++ BirthdayAppB/birthdayapp/src/test/java/com/serbay/birthdayapp/BirthdayappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serbay.birthdayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
