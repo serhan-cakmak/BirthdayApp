03e53aa3190c867f56f6f5fa72c5eacab9c8e0d6
serhan-cakmak <104589882+serhan-cakmak@users.noreply.github.com>
2022-09-14 00:31:46 +0300

    axios change

--- BirthdayAppF/src/api/axios.js
+++ BirthdayAppF/src/api/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL : 'http://localhost:8080'
+    baseURL : 'http://birthday-env.eba-pnmx93hn.us-east-1.elasticbeanstalk.com'
 });
